aa00f8ba8317fe3b6ff4b3e26eaeb94e12ab0b1d
Anano Topuria <anano.topuriaa@gmail.com>
2026-09-13 22:59:37 +0400

    feat: add user schema and users module

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({
+  timestamps: true,
+  versionKey: false,
+})
+export class User {
+  @Prop({
+    required: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 50,
+  })
+  username: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+    select: false,
+  })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.index({ email: 1 }, { unique: true });
