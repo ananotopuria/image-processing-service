f9832d64035598607f7e69f4911121bd9e57f2be
Anano Topuria <anano.topuriaa@gmail.com>
2026-09-17 15:13:07 +0400

    feat: add AWS S3 image storage

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -2,19 +2,21 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from '../auth/auth.module';
+import { S3Module } from '../s3/s3.module';
 import { ImagesController } from './images.controller';
 import { ImagesService } from './images.service';
 import { Image, ImageSchema } from './schemas/image.schema';
 
 @Module({
   imports: [
-    AuthModule,
     MongooseModule.forFeature([
       {
         name: Image.name,
         schema: ImageSchema,
       },
     ]),
+    AuthModule,
+    S3Module,
   ],
   controllers: [ImagesController],
   providers: [ImagesService],
---
+++ src/s3/s3.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  DeleteObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
+
+@Injectable()
+export class S3Service {
+  private readonly s3Client: S3Client;
+  private readonly bucketName: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.bucketName = this.configService.getOrThrow<string>('AWS_S3_BUCKET');
+
+    this.s3Client = new S3Client({
+      region: this.configService.getOrThrow<string>('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.getOrThrow<string>('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.getOrThrow<string>(
+          'AWS_SECRET_ACCESS_KEY',
+        ),
+      },
+    });
+  }
+
+  async uploadFile(
+    key: string,
+    buffer: Buffer,
+    contentType: string,
+  ): Promise<void> {
+    await this.s3Client.send(
+      new PutObjectCommand({
+        Bucket: this.bucketName,
+        Key: key,
+        Body: buffer,
+        ContentType: contentType,
+      }),
+    );
+  }
+
+  async deleteFile(key: string): Promise<void> {
+    await this.s3Client.send(
+      new DeleteObjectCommand({
+        Bucket: this.bucketName,
+        Key: key,
+      }),
+    );
+  }
+}
